35a2ca26ae3227dc83d87fe7739a729a07574bc4
nmasters <nmasters52@gmail.com>
2025-09-05 05:40:04 -0400

    add items to the cart state

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useCart } from '../context/CartContext'
 
 const ProductCard = ({ product }) => {
+  const {addToCart} = useCart();
+
   return (
     <div className="bg-white rounded-lg shadow p-4 flex flex-col">
         <img 
@@ -11,6 +14,12 @@ const ProductCard = ({ product }) => {
         <h2 className="font-semibold text-xl">{product.name}</h2>
         <p className="text-gray-500 text-sm mb-2">{product.description}</p>
         <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
+        <button 
+          className="bg-blue-600 mt-3 py-2 px-4 text-white transition rounded cursor-pointer hover:bg-blue-700"
+          onClick={() => addToCart(product)}
+        > 
+          Add To Cart
+        </button>   
     </div>
   )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,13 +1,29 @@
 import {useState, createContext, useContext} from 'react'
 
-
 const CartContext = createContext();
 
 export function CartProvider({children}) {
     const [cart, setCart] = useState([]);
 
+    const addToCart = (product) => {
+        setCart((prev) => {
+            //find if the product past matches the previous products in the cart 
+            const existingItem = prev.find((item) => item.id === product.id);
+
+            //if the previous item in the cart matches, add one to the qty. else return the item as is.
+            if (existingItem) {
+               return prev.map((item) =>
+                    item.id === product.id ? {...item, qty: item.qty + 1} : item
+                );
+            }
+
+            //if the item was not in the cart it be returned with the qty: 1 added to its data structure. 
+            return [...prev, {...product, qty: 1}];
+            });
+        };
+
     return (
-        <CartContext.Provider value={{cart, setCart}}>
+        <CartContext.Provider value={{cart, addToCart}}>
             {children}
         </CartContext.Provider>
     )
